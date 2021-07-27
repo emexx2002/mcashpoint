575378f036e6ac068b144fe71cab4372734c8a58
Agnes Ndip <speak2jedi@gmail.com>
2021-07-27 17:31:10 +0100

    header

--- src/Views/template/dashboardtemplate/index.js
+++ src/Views/template/dashboardtemplate/index.js
@@ -1,39 +1,33 @@
-import React, { Component } from 'react'
-import Header from '../../../Components/Header'
-import SideNav from '../../../Components/SideNav'
+import React, { Component } from "react";
+import Header from "../../../Components/Header";
+import SideNav from "../../../Components/SideNav";
 import "./style.css";
-import { createHashHistory } from 'history'
+import { createHashHistory } from "history";
 import { withRouter } from "react-router-dom";
 import { Container, Row, Col } from "react-bootstrap";
 
+class DashboardTemplate extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      showSideNav: false,
+    };
+  }
 
-
- class DashboardTemplate extends Component {
-    constructor(props){
-        super(props);
-
-        this.state = {
-            showSideNav: false,
-        }
-    }
-
-    render() { 
-        return (
-            <div className="dashboardTemplate-wrap">
-                <div className='row-wrapper'>
-                 <SideNav />   
-                </div>
-
-                <div className="mainwrapper">
-                <Header />
-                {this.props.children}  
-                </div>
-            </div>
-        )
-    }
-
-
+  render() {
+    return (
+      <div className="dashboardTemplate-wrap">
+        <div className="row-wrapper">
+          <SideNav />
+        </div>
+        <div className="mainwrapper">
+          <Header />
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default withRouter(DashboardTemplate)
+export default withRouter(DashboardTemplate);
