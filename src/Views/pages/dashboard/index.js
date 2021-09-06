7a400f26b0a0967f4e020279528d67a1f137cef6
Agnes Ndip <speak2jedi@gmail.com>
2021-09-06 15:09:12 +0100

    refactor styling

--- src/Views/pages/dashboard/index.js
+++ src/Views/pages/dashboard/index.js
@@ -120,106 +120,72 @@ const DashBoard = (props) => {
 
           {/* <div className="transaction-graph-wrapper"> */}
           <div className="line-and-details">
-            <div className="chart-bg">
-              <LineChart />
+           <div className="chart-status">
+              <div className="chart-bg">
+                <Barchart />
+              </div>
+              <div className="transaction-graph-inner">
+                <div className="transaction-details a">
+                  <p>Successful</p>
+                  <h6 className="success-text">
+                    {successful_value
+                      ? successful_value.toLocaleString()
+                      : "#0000"}
+                    (
+                    {successful_volume ? successful_volume.toLocaleString() : "0"}
+                    )
+                  </h6>
+                </div>
+                <div className="transaction-details">
+                  <p>Failed</p>
+                  <h6 className="failure-text ">
+                    {failed_value ? failed_value.toLocaleString() : "#000"}(
+                    {failed_volume ? failed_volume.toLocaleString() : "0"})
+                  </h6>
+                </div>
+                <div className="transaction-details b">
+                  <p>Agent Registered</p>
+                  <h6>{numberOfAgents ? numberOfAgents : "00"}</h6>
+                </div>
+              </div>
             </div>
-            <div className="transaction-graph-inner">
-              <div className="transaction-details a">
-                <p>Successful</p>
-                <h6 className="success-text">
-                  {successful_value
-                    ? successful_value.toLocaleString()
-                    : "#0000"}
-                  (
-                  {successful_volume ? successful_volume.toLocaleString() : "0"}
-                  )
-                </h6>
+            <div className="dough-agent">
+              <div className="dougnut-wrapper">
+                <div className="dougnut-chart">
+                  <Doughnut />
+                </div>
+                <div className="transaction-types">
+                  {transactiondetails}
+                </div>
               </div>
-              <div className="transaction-details">
-                <p>Failed</p>
-                <h6 className="failure-text ">
-                  {failed_value ? failed_value.toLocaleString() : "#000"}(
-                  {failed_volume ? failed_volume.toLocaleString() : "0"})
-                </h6>
-              </div>
-              <div className="transaction-details b">
-                <p>Agent Registered</p>
-                <h6>{numberOfAgents ? numberOfAgents : "00"}</h6>
-              </div>
-            </div>
+               <div className="bar-graph-wrapper">
+              {/* <div className="barchart-bg">
+                <Barchart />
+              </div> */}
+              {name == "ADMIN" ||
+              name == "Senior Management " ||
+              name == "Product" ? (
+                <div className="daily-per-agent">
+                  <div id="daily-header">Daily Top Performing Agents</div>
 
-            <div className="dougnut-wrapper">
-              <div className="dougnut-chart">
-                <Doughnut />
-              </div>
-              <div className="transaction-types">
-                {transactiondetails}
-
-                {/* <div className="transaction-types-wrapper">
-                  <div>
-                    <div className="fundtransfer-dot"></div>Funds Transfer
-                  </div>
-                  <div>20(78.5%)</div>
+                  {mostPerformingAgent
+                    .slice(0, 5)
+                    .map((PerformingAgent, index) => (
+                      <div>
+                        <li>{PerformingAgent.businessName}</li>
+                      </div>
+                    ))}
                 </div>
-                <div className="transaction-types-wrapper">
-                  <div>
-                    <div className="creditpurse-dot"></div>Credit Purse
-                  </div>
-                  <div>20(78.5%)</div>
-                </div>
-                <div className="transaction-types-wrapper">
-                  <div>
-                    <div className="gotv-dot"></div>GOtv
-                  </div>
-                  <div>20(78.5%</div>
-                </div>
-                <div className="transaction-types-wrapper">
-                  <div>
-                    <div className="recharge-dot"></div>Recharge Top Up
-                  </div>
-                  <div>20(78.5%</div>
-                </div>
-                <div className="transaction-types-wrapper">
-                  <div>
-                    <div className="startime-dot"></div> Star Times
-                  </div>
-                  <div>20(78.5%</div>
-                </div>
-                <div className="transaction-types-wrapper">
-                  <div>
-                    <div className="Agentransfer-dot"></div>Agent Transfer
-                  </div>
-                  <div>20(78.5%)</div>
-                </div> */}
-              </div>
-            </div>
+              ) : (
+                ""
+              )}
           </div>
-
-          <div className="bar-graph-wrapper">
-            <div className="barchart-bg">
-              <Barchart />
             </div>
-            {name == "ADMIN" ||
-            name == "Senior Management " ||
-            name == "Product" ? (
-              <div className="daily-per-agent">
-                <div id="daily-header">Daily Top Performing Agents</div>
 
-                {mostPerformingAgent
-                  .slice(0, 5)
-                  .map((PerformingAgent, index) => (
-                    <div>
-                      <li>{PerformingAgent.businessName}</li>
-                    </div>
-                  ))}
-              </div>
-            ) : (
-              ""
-            )}
+         
           </div>
         </div>
 
-        {/* </div> */}
       </div>
     </DashboardTemplate>
   );
